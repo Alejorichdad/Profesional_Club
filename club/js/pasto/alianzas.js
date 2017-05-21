433ec8e1eaf44f63e7eee820997452c9446b63b5
Alejandro Calderon <topplanner.com.co@gmail.com>
2017-05-20 19:48:26 -0500

    Este commit se actualiza pasto total

--- club/js/pasto/alianzas.js
+++ club/js/pasto/alianzas.js
@@ -214,6 +214,278 @@ $(this).removeClass("open");
 });
 
 
+//angelex
+
+$(function(){
+$("#activoangelex").show();
+$(".angelex-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".angelex-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".angelex-titulo").removeClass("open");
+$(".angelex-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+//angelex
+
+$(function(){
+$("#activocasaoptica").show();
+$(".casaoptica-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".casaoptica-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".casaoptica-titulo").removeClass("open");
+$(".casaoptica-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+//computronix
+
+$(function(){
+$("#activocomputronix").show();
+$(".computronix-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".computronix-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".computronix-titulo").removeClass("open");
+$(".computronix-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+
+//chaparral
+
+$(function(){
+$("#activochaparral").show();
+$(".chaparral-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".chaparral-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".chaparral-titulo").removeClass("open");
+$(".chaparral-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+
+//morasurco
+
+$(function(){
+$("#activomorasurco").show();
+$(".morasurco-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".morasurco-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".morasurco-titulo").removeClass("open");
+$(".morasurco-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+//sindamanoy
+
+$(function(){
+$("#activosindamanoy").show();
+$(".sindamanoy-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".sindamanoy-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".sindamanoy-titulo").removeClass("open");
+$(".sindamanoy-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+//muscle
+
+$(function(){
+$("#activomuscle").show();
+$(".muscle-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".muscle-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".muscle-titulo").removeClass("open");
+$(".muscle-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+//karibeña
+
+$(function(){
+$("#activokaribena").show();
+$(".karibena-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".karibena-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".karibena-titulo").removeClass("open");
+$(".karibena-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+////orthomax
+
+$(function(){
+$("#activoorthomax").show();
+$(".orthomax-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".orthomax-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".orthomax-titulo").removeClass("open");
+$(".orthomax-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+
+$(function(){
+$("#activokaribena").show();
+$(".karibena-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".karibena-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".karibena-titulo").removeClass("open");
+$(".karibena-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
+
+
+
+$(function(){
+$("#activopoledance").show();
+$(".poledance-titulo").click(function(e){
+
+e.preventDefault();
+
+var contenido=$(this).next(".poledance-content");
+
+if(contenido.css("display")=="none"){ //open
+
+$(".poledance-titulo").removeClass("open");
+$(".poledance-content").slideUp(550);
+contenido.slideDown(550);
+$(this).addClass("open");
+}
+else{ //close
+contenido.slideUp(550);
+$(this).removeClass("open");
+}
+});
+});
 
 
 });
